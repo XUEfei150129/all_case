dacf3653e6c2ef46e70a4f8d85eb35006d0bc4d0
“XUEfei150129” <1010029946@qq.com>
2019-03-29 13:55:44 +0800

    提交主流程代码

---
+++ case/api_case/SALE/__init__.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env/python3
+# coding=utf-8
+# @Time : 2019/3/27 9:02
+# @Author : XueFei
---
+++ case_lib/api_case_lib/MAIN_PROCESS/__init__.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env/python3
+# coding=utf-8
+# @Time : 2019/3/27 9:48
+# @Author : XueFei
